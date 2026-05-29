a528ddc8caffd254924265052d7d6c4d0c642445
Anees Fathima <anees17.fathima@gmail.com>
2026-05-29 22:12:26 +0530

    Added GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/She_Can_Foundation_WebDevelopment_Internship/',
   plugins: [react(), tailwindcss()],
 })
